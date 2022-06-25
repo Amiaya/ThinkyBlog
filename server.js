b3a9b8201589f0fda6da6ec9435efa8e5dfec3aa
Amiaya <amiayaprime@gmail.com>
2022-06-25 14:05:42 +0100

    minor changes server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const port = 4000 || process.env.PORT
+const port = process.env.PORT || 4000
 
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
