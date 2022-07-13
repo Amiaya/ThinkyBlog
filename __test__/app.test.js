b2914f0631789ea433602a1cf1cba6b4f8014123
Amiaya <amiayaprime@gmail.com>
2022-07-13 21:56:18 +0100

    Second test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,76 +1,60 @@
 const app = require('../app')
-const supertest = require('supertest')
-const mongoose = require('mongoose')
+const request = require('supertest')
 const dotenv = require('dotenv')
-const { MongoMemoryServer } = require('mongodb-memory-server')
-
-const request = supertest(app);
+const userValidator = require('../schema/userValidator')
+const User = require('../models/User')
+const Category = require('../models/category')
 
 dotenv.config()
 const user = {
     email: "charles@gmail.com",
     password: "qazwsxed"
 }
+const  userInput = {
+    FullName:"Charles Adebeyi",
+    email: "charles.A@gmail.com",
+    password: "qazwsxed"
+}
 
+const categoryInput = [
+    {name: 'Category' },
+    {name: 'Entertainment'},
+    {name: 'Games'},
+    {name: 'Internet'},
+    {name: 'Technology'},
+    {name: 'Society'}
+]
 
 describe('user', () => {
-    let db
-    beforeAll(async() => {
-        const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
-
-        db = mongoose.connect(DB, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-        }).then(()=> console.log('DB connection successful!'))
-         
-          
-    })
-
-    afterAll(async () => {
-        await db.close();
-      });
     describe('user registration', () =>{
-        describe('Given the user password and email are valid', () =>{
-            it('should return the user reponse 200', async () => {
-                const res = await request.post('/ThinkyBlog/login', user)
-                expect(res.status).toBe(200)
+        describe('Given the user Fullname, password and email is valid', () => {
+            it('should return the user response with 201', async ()=> {
+                jest.spyOn(userValidator, 'validateAsync').mockResolvedValue(userInput)
+                jest.spyOn(User, 'create').mockResolvedValue(userInput)
+            
+                const {statusCode} = await request(app).post('/ThinkyBlog/signup').send(userInput)
+                expect(statusCode).toBe(201)
             })
         })
-        describe('Given the user password and email do not match', () =>{
-            it('should return the user reponse 400', () => {
+        // describe('Given the user email and password is valid' , () => {
+        //     it('should return the user reponsew with 200', async ()=> {
+        //         jest.spyOn(User, 'findOne').mockResolvedValue(user)
+
+        //         const {statusCode} = await (await request(app).post('/ThinkyBlog/login')).send(user)
+        //         expect(statusCode).toBe(200)
                 
-            })
+        //     })
+        // })
+    })
+})
+
+describe('category', () => {
+    describe('/POST Category', () => {
+        it('should return response with 201', async () => {
+            jest.spyOn(Category, 'create').mockResolvedValue(categoryInput)
+        
+            const {statusCode} = await request(app).post('/ThinkyBlog/category').send(categoryInput)
+            expect(statusCode).toBe(201)
         })
     })
 })
-// describe('POST /ThinkyBlog/login' ,() => {
-//     beforeAll(async () => {
-//         const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
-
-//         mongoose.connect(DB, { 
-//         useNewUrlParser: true, 
-//         useUnifiedTopology: true
-//         }).then(()=> console.log('DB connection successful!'))
-
-//       });    it('should have a statusCode of 200', async ()=> {
-//         const response = await request(app).post("/ThinkyBlog/login").send(user)
-//         expect(response.statusCode).toEqual(200)
-       
-//     })
-
-//     it('should return an error if a parameter is missing', async ()=> {
-       
-//         const response = await request(app).post("/ThinkyBlog/login").send({
-//             email: "charles@gmail.com",
-//             password: ""
-//         })
-//         expect(response.statusCode).toEqual(401)
-//     })
-
-//     afterAll(async  (done) => {
-//         mongoose.disconnect();
-//         mongoose.connection.close()
-//         done();
-//     });
-// })
-
